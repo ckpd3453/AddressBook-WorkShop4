d7ffe5164925192552b22984b53f4b31fa2c630a
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-04-01 08:28:20 +0530

    Added UC-1 Problem to get the person details

---
+++ src/main/java/AddressBook.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AddressBook {
+	// UC1
+	static Scanner sc = new Scanner(System.in);
+	static List<Person> list = new ArrayList<Person>();
+	static Person person;
+
+}
+
